df3f5821941eab045161ac3b7adbd9f53ef18ac5
duracell27 <volodymyrshmidt@gmail.com>
2021-04-30 10:56:34 +0300

    1 lesson done

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,7 @@ import {WelcomeStep} from '../components/steps/WelcomeStep'
 export default function Home() {
   return (
     <div >
-      <Head>
-        <title>Main page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      
       <WelcomeStep/>
     </div>
   )
