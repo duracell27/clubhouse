850038b8f38a6e16ab3585f5428f1dbf088b5d0c
duracell27 <volodymyrshmidt@gmail.com>
2021-05-04 18:05:07 +0300

    lesson 3 work

--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -1,11 +1,17 @@
+import { Button } from "../components/Button";
+import { ConversationCard } from "../components/ConversationCard";
 import { Header } from "../components/Header";
 
 export default function Rooms() {
   return (
     <>
       <Header />
-      <div className="container">
-        <h1>Rooms</h1>
+      <div className="container mt-40 d-flex align-items-center justify-content-between">
+        <h1>All conversations</h1>
+        <Button color='green'>+ Start room</Button>
+      </div>
+      <div className="mt-20">
+        <ConversationCard />
       </div>
     </>
   );
